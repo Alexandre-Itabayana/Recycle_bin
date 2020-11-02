d28773b483cecd28b3d100e87d39fbcd902f1407
Alexandre-Itabayana <picanha@gmail.com>
2020-11-02 14:55:37 -0300

    Initial commit

---
+++ src/Usuario.java
@@ -0,0 +1,61 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Picanha-PC
+ */
+public class Usuario {
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Date getData_inicio() {
+        return data_inicio;
+    }
+
+    public void setData_inicio(Date data_inicio) {
+        this.data_inicio = data_inicio;
+    }
+
+    public Date getData_final() {
+        return data_final;
+    }
+
+    public void setData_final(Date data_final) {
+        this.data_final = data_final;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    private String login;
+    private Date data_inicio;
+    private Date data_final;
+    private String senha;
+    private String email;
+     
+}
