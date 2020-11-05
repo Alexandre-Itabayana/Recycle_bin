e396468327ac49527c1afeac55a833733a7b2efe
tuliomangabeira <tuliomangabeira@hotmail.com>
2020-11-05 15:51:21 -0300

    Update Empresa.java

--- src/Empresa.java
+++ src/Empresa.java
@@ -1,19 +1,92 @@
-/* Essa classe faz login com Empresa e chama método Serviço*/ 
+/* Essa classe faz login com Empresa e chama mï¿½todo Serviï¿½o*/
 
 public class Empresa {
    private long cnpj;
-   private float usuariosenha; 
+   private float usuariosenha;
+   private Usuario usuario;
+   private String endereco;
+   private int complemento;
+   private String bairro;
+   private String uf;
+   private String cidade;
+   private int cep;
+   private String pais;
+   private int telefone;
 
-	public Empresa (long num, float user) {
-	cnpj = num;
+   public Usuario getUsuario() {
+     return usuario;
+  }
+
+  public void setUsuario(Usuario usuario) {
+      this.usuario = usuario;
+  }
+
+  public String getEndereco() {
+    return endereco;
+}
+
+  public void setEndereco(String endereco) {
+      this.endereco = endereco;
+  }
+
+  public int getComplemento() {
+      return complemento;
+    }
+
+  public void setComplemento(int complemento) {
+      this.complemento = complemento;
+  }
+
+  public String getBairro() {
+      return bairro;
+  }
+
+  public void setBairro(String bairro) {
+      this.bairro = bairro;
+  }
+
+  public String getUf() {
+      return uf;
+  }
+
+  public void setUf(String uf) {
+      this.uf = uf;
+  }
+
+  public String getCidade() {
+      return cidade;
+  }
+
+  public void setCidade(String cidade) {
+      this.cidade = cidade;
+  }
+
+  public int getCep() {
+      return cep;
+  }
+
+  public void setCep(int cep) {
+      this.cep = cep;
+  }
+
+  public String getPais() {
+      return pais;
+  }
+
+  public void setPais(String pais) {
+      this.pais = pais;
+  }
+
+
+   public Empresa (long num, float user) {
+        cnpj = num;
 	usuariosenha = user;
-	}
-	
-	public void RegistraColeta (long num){
+    }
+
+    public void RegistraColeta (long num){
 		if (usuariosenha == 0)
 			cnpj = cnpj + 1;
 		/* cnpf Registra Coleta no banco de dados*/
-		
-	}
+    }
 
 }
